881b4192152eb1ee757018c5b67087848719c47d
Timo Vogel <vogeltimo@outlook.de>
2020-11-08 10:45:05 +0100

    Restart applicatoin after chocolatey installation (not tested yet)

--- SetupTool/MainWindow.cs
+++ SetupTool/MainWindow.cs
@@ -224,6 +224,7 @@ namespace SetupTool
             string installCommand = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))";
             
             executeShellCommand(installCommand);
+            Application.Restart(); //TODO: Test
         }
 
         /// <summary>
