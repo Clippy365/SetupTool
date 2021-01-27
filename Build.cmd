a886414523d9a364714e74d193c50dc1b673e2ba
Timo Vogel <vogeltimo@outlook.de>
2021-01-28 00:16:53 +0100

    Change Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,4 @@
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\SetupTool.sln -property:Configuration=Release
 
-xcopy .\SetupTool\bin\Release\SetupTool.exe .
-xcopy .\SetupTool\bin\Release\Newtonsoft.Json.dll .
+xcopy .\SetupTool\bin\Release\SetupTool.exe . /Y
+xcopy .\SetupTool\bin\Release\Newtonsoft.Json.dll . /Y
