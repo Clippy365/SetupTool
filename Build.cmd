0fe42e7ebe01228dbada7b19f69d7664a37323be
Timo Vogel <vogeltimo@outlook.de>
2021-02-05 00:32:36 +0100

    Change Buildscript to resolve dependency

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,18 @@
+@echo off
+
+if not exist "nuget.exe" (
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', 'nuget.exe')"
+)
+
+nuget.exe install Newtonsoft.Json -OutputDirectory packages
+
+echo.
+echo --------------------------------------------------------
+echo.
+
+rem Remove "coming-from-the-internet-flag" recursively
+powershell "Get-ChildItem -Recurse | Unblock-File"
+
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\SetupTool.sln -property:Configuration=Release
 
 xcopy .\SetupTool\bin\Release\SetupTool.exe . /Y
