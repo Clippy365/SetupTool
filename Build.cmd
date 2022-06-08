d63e6efdaa3c95726863c34c71a34237d6458090
Timo Vogel <vogeltimo@outlook.de>
2022-06-08 22:18:35 +0200

    Switch to Nuget 13.0.1

--- Build.cmd
+++ Build.cmd
@@ -4,7 +4,7 @@ if not exist "nuget.exe" (
 powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', 'nuget.exe')"
 )
 
-nuget.exe install Newtonsoft.Json -OutputDirectory packages
+nuget.exe install Newtonsoft.Json -OutputDirectory packages -Version 13.0.1
 
 echo.
 echo --------------------------------------------------------
